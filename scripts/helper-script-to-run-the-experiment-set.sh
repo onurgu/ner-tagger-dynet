622af5223b394abde1990bdf9e65804a84fe5394
onur gungor <onurgu@gmail.com>
2017-12-11 19:11:35 +0300

    helper script to run the experiment set

--- scripts/helper-script-to-run-the-experiment-set.sh
+++ scripts/helper-script-to-run-the-experiment-set.sh
@@ -44,34 +44,31 @@ for imode in 0 1 2 ; do
 		test_filepath=turkish/gungor.ner.test.14.only_consistent \
 		experiment_name=${experiment_name} ;
 	else
-		for amodels in 2 ; do
-			if [[ $amodels == 3 ]]; then
-				cd ${ner_tagger_root} && \
-				source /usr/local/bin/virtualenvwrapper.sh && workon dynet && \
-				source environment-variables && python control_experiments.py -m joint_ner_and_md with \
-				active_models=$amodels \
-				integration_mode=$imode \
-				multilayer=1 \
-				shortcut_connections=1 \
-				dynet_gpu=0 \
-				embeddings_filepath="" \
-				train_filepath=turkish/gungor.ner.train.14.only_consistent \
-				dev_filepath=turkish/gungor.ner.dev.14.only_consistent \
-				test_filepath=turkish/gungor.ner.test.14.only_consistent \
-				experiment_name=${experiment_name} ;
-			else
-				cd ${ner_tagger_root} && \
-				source /usr/local/bin/virtualenvwrapper.sh && workon dynet && \
-				source environment-variables && python control_experiments.py -m joint_ner_and_md with \
-				active_models=$amodels \
-				integration_mode=$imode \
-				dynet_gpu=0 \
-				embeddings_filepath="" \
-				train_filepath=turkish/gungor.ner.train.14.only_consistent \
-				dev_filepath=turkish/gungor.ner.dev.14.only_consistent \
-				test_filepath=turkish/gungor.ner.test.14.only_consistent \
-				experiment_name=${experiment_name} ;
-			fi
-		done
+		cd ${ner_tagger_root} && \
+		source /usr/local/bin/virtualenvwrapper.sh && workon dynet && \
+		source environment-variables && python control_experiments.py -m joint_ner_and_md with \
+		active_models=2 \
+		integration_mode=2 \
+		multilayer=1 \
+		shortcut_connections=1 \
+		dynet_gpu=0 \
+		embeddings_filepath="" \
+		train_filepath=turkish/gungor.ner.train.14.only_consistent \
+		dev_filepath=turkish/gungor.ner.dev.14.only_consistent \
+		test_filepath=turkish/gungor.ner.test.14.only_consistent \
+		experiment_name=${experiment_name} ;
+
+		cd ${ner_tagger_root} && \
+		source /usr/local/bin/virtualenvwrapper.sh && workon dynet && \
+		source environment-variables && python control_experiments.py -m joint_ner_and_md with \
+		active_models=2 \
+		integration_mode=2 \
+		dynet_gpu=0 \
+		embeddings_filepath="" \
+		train_filepath=turkish/gungor.ner.train.14.only_consistent \
+		dev_filepath=turkish/gungor.ner.dev.14.only_consistent \
+		test_filepath=turkish/gungor.ner.test.14.only_consistent \
+		experiment_name=${experiment_name} ;
+
 	fi ;
 done
